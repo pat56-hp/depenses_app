5daa08341e5f5692bc35369f8eec7e726b523347
pat56-hp <patrickkouassi7@gmail.com>
2025-04-03 12:15:55 +0000

    Update api route

--- lib/core/routes/api_end_point.dart
+++ lib/core/routes/api_end_point.dart
@@ -1,5 +1,5 @@
 abstract class ApiEndPoint {
-  static const String apiBaseUrl = 'http://192.168.1.36:8000';
+  static const String apiBaseUrl = 'http://192.168.1.35:8000';
   static const String apiUrl = '$apiBaseUrl/api/v1';
   static const String login = '/login';
   static const String register = '/register';
